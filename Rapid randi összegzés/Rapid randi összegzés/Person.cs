28355aa03c6abbc089714cc28a82072007f10c03
OcskoMark <ocsko.mark@gmail.com>
2022-02-13 23:43:35 +0100

    Az összefésülés és e-mailek generálása megvalósítva.

--- Rapid randi összegzés/Rapid randi összegzés/Person.cs
+++ Rapid randi összegzés/Rapid randi összegzés/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,11 @@ namespace Rapid_randi_összegzés
 		private List<string> sympathics = new List<string>();
 		private int getSympathics = 0;
 		private List<Person> matches = new List<Person>();
+		private Boolean isGetSympathics;
 
 		public Person(string iD, string nickName, string name, string phoneNumber, string email, DateTime birthDate, string sex)
 		{
-			ID1 = iD;
+			ID1 = iD.ToUpper();
 			this.NickName = nickName;
 			this.Name = name;
 			this.PhoneNumber = phoneNumber;
@@ -54,6 +56,71 @@ namespace Rapid_randi_összegzés
 		public bool IsWoman { get => isWoman; set => isWoman = value; }
 		public List<string> Sympathics { get => sympathics; set => sympathics = value; }
 		public int GetSympathics { get => getSympathics; set => getSympathics = value; }
+		public bool IsGetSympathics { get => isGetSympathics; set => isGetSympathics = value; }
 		internal List<Person> Matches { get => matches; set => matches = value; }
+
+		public void Writer(string path)
+		{
+			StreamWriter writer = new StreamWriter(path + "\\E-mailek\\" + name + ".txt");
+
+			writer.WriteLine("Kedves " + nickName + "!");
+			writer.WriteLine();
+			writer.WriteLine("Örülünk, hogy részt vettél a Rapid randin. Reméljük, jól érezted magad!");
+			writer.WriteLine();
+			
+			if (matches.Count == 0)
+			{
+				writer.WriteLine("Sajnos senkivel sem volt közös találatod.");
+				writer.WriteLine();
+			}
+			else
+			{
+				writer.WriteLine("Gratulálunk, " + matches.Count + " személlyel találtátok kölcsönösen szimpatikusnak egymást.");
+				writer.WriteLine("Küldöm a másik elérhetőségeit:");
+				writer.WriteLine();
+				foreach (Person person in matches)
+				{
+					writer.WriteLine("Név: " + person.Name);
+					writer.WriteLine("E-mail cím: " + person.Email);
+					writer.WriteLine("Telefonszám: " + person.PhoneNumber);
+					writer.WriteLine();
+				}
+			}
+
+			if (isGetSympathics)
+			{
+				if (getSympathics == 0)
+				{
+					writer.WriteLine("Sajnos 0 beszélgetőpartnered jelölt szimpatikusnak. Ne csüggedj, értékes vagy! A beszélgetőpartnereid csak egy nagyon kicsi halmaza a másik nemnek.");
+				}
+				else
+				{
+					writer.WriteLine("Gratulálok, " + getSympathics + " beszélgetőpartnered jelölt szimpatikusnak.");
+				}
+
+				writer.WriteLine();
+			}
+
+			writer.WriteLine("Fontos számunkra a véleményed, ezért kérlek, töltsd ki teljesen anonim kérdőívünket az eseményről, hogy a jövőbeli hasonló eseményeink szervezésekor a Te tapasztalataidat is figyelembe tudjuk venni!");
+			writer.WriteLine("A kérdőívet az alábbi linken érheted el:");
+			writer.WriteLine("");
+			writer.WriteLine("");
+			writer.WriteLine("Sok sikert kívánunk a további ismerkedéshez!");
+			writer.WriteLine("");
+			writer.WriteLine("Üdvözlettel:");
+			writer.WriteLine("a Szervezőcsapat nevében, Ocskó Márk");
+
+			writer.Close();
+		}
+
+		public void addSympathics (string ID)
+		{
+			sympathics.Add(ID);
+		}
+
+		public void addMatches (Person person)
+		{
+			matches.Add(person);
+		}
 	}
 }
--- Rapid randi összegzés/Rapid randi összegzés/Program.cs
+++ Rapid randi összegzés/Rapid randi összegzés/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,157 @@ namespace Rapid_randi_összegzés
 {
 	class Program
 	{
+		static Dictionary<string, Person> readPersons(string sourcePath)
+		{
+			Dictionary<string, Person> persons = new Dictionary<string, Person>();
+
+			StreamReader reader = new StreamReader(sourcePath + "\\Résztvevők.csv");
+
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				string[] pieces = line.Split(';');
+				string[] birthDatas = pieces[4].Split('.');
+
+				DateTime birthDate = new DateTime(Convert.ToInt32(birthDatas[0]), Convert.ToInt32(birthDatas[1]), Convert.ToInt32(birthDatas[2]));
+
+				Person person = new Person(pieces[0], pieces[1], pieces[2], pieces[6], pieces[5], birthDate, pieces[3]);
+
+				persons.Add(pieces[0], person);
+			}
+
+			reader.Close();
+
+			return persons;
+		}
+
+		static Dictionary<string, Person> readSympathics (string sourcePath, Dictionary<string, Person> persons)
+		{
+			StreamReader reader = new StreamReader(sourcePath + "\\Szimpátiák.csv");
+
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				string[] pieces = line.Split(';');
+
+				if (pieces[1].ToUpper() == "I")
+				{
+					persons[pieces[0].ToUpper()].IsGetSympathics = true;
+				}
+				else
+				{
+					persons[pieces[0].ToUpper()].IsGetSympathics = false;
+				}
+
+				for (int i = 2; i < pieces.Length; i++)
+				{
+					if (pieces[i] == "")
+					{
+						break;
+					}
+					else
+					{
+						persons[pieces[0].ToUpper()].addSympathics(pieces[i].ToUpper());
+						persons[pieces[i].ToUpper()].GetSympathics++;
+					}
+				}
+			}
+
+			reader.Close();
+
+			return persons;
+		}
+		static Dictionary<string, Person> calculateMatches (Dictionary<string, Person> persons)
+		{
+			foreach (KeyValuePair<string, Person> item in persons)
+			{
+				foreach(string ID in item.Value.Sympathics)
+				{
+					if (persons[ID].Sympathics.Contains(item.Value.ID1))
+					{
+						persons[item.Value.ID1].addMatches(persons[ID]);
+					}
+				}
+			}
+
+			return persons;
+		}
+
+		static void Statistics (Dictionary<string, Person> persons, string destinationPath)
+		{
+			int sumAge = 0;
+			int sumManAge = 0;
+			int sumWomanAge = 0;
+			int sumSympathics = 0;
+			int sumManSympathics = 0;
+			int sumWomanSympathics = 0;
+			int maxManSympathics = 0;
+			int minManSympathics = 100;
+			int maxWomanSympathics = 0;
+			int minWomanSympathics = 100;
+			int numberOfWomans = 0;
+
+			foreach (KeyValuePair<string, Person> item in persons)
+			{
+				sumAge += item.Value.Age;
+				sumSympathics += item.Value.Sympathics.Count;
+				numOfWomans++;
+
+				if (item.Value.IsWoman)
+				{
+					sumWomanAge += item.Value.Age;
+					sumWomanSympathics += item.Value.Sympathics.Count;
+
+					if (maxWomanSympathics < item.Value.Sympathics.Count)
+					{
+						maxWomanSympathics = item.Value.Sympathics.Count;
+					}
+					else if (minWomanSympathics > item.Value.Sympathics.Count)
+					{
+						minWomanSympathics = item.Value.Sympathics.Count;
+					}
+				}
+				else
+				{
+					sumManAge += item.Value.Age;
+					sumManSympathics += item.Value.Sympathics.Count;
+
+					if (maxManSympathics < item.Value.Sympathics.Count)
+					{
+						maxManSympathics = item.Value.Sympathics.Count;
+					}
+					else if (minManSympathics > item.Value.Sympathics.Count)
+					{
+						minManSympathics = item.Value.Sympathics.Count;
+					}
+				}
+			}
+
+			int numberOfPersons = persons.Count;
+			int numberOfMans = numberOfPersons - numberOfWomans;
+			double avgAge = sumAge / numberOfPersons;
+			double avgManAge = sumManAge / numberOfMans;
+			double avgWomanAge = sumWomanAge / numberOfWomans;
+			double avgSympathics = sumSympathics / numberOfPersons;
+			double avgManSympathics = sumManSympathics / numberOfMans;
+			double avgWomanSympathics = sumWomanSympathics / numberOfWomans;
+		}
+
+
 		static void Main(string[] args)
 		{
-			
+			string sourcePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName;
+			string destinationPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName + "\\Eredmények";
+
+			Dictionary<string, Person> persons = readPersons(sourcePath);
+			persons = readSympathics(sourcePath, persons);
+			persons = calculateMatches(persons);
+
+			foreach (KeyValuePair<string, Person> item in persons)
+			{
+				item.Value.Writer(destinationPath);
+			}
+
 		}
 	}
 }
